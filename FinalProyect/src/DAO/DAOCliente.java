628be31bc50a6352acdb8bbec6c9f03ba5eb12d3
Irving Poot <eduardopoot17@gmail.com>
2021-02-10 00:10:12 -0600

    DAO

---
+++ FinalProyect/src/DAO/DAOCliente.java
@@ -0,0 +1,84 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+/**
+ *
+ * @author Irving Poot
+ */
+public class DAOCliente extends DAOGeneral<Cliente>{
+    public DAOCliente() {
+    }
+
+    public int agregar(Cliente e) throws SQLException {
+        int numFilas = 0;
+        Connection con = getConeccion();
+
+        String orden = "INSERT INTO cliente (clave, nombre, fechaIngreso, fechaSalida, numhab, comida, correo)"+
+                "VALUES ("+ e.getClaveCliente()+ ",'" + e.getNombre()+ "','" + 
+                e.getFechaIngreso().toString() + "',"+ e.isActivo()+")";
+
+        Statement sentencia = con.createStatement();
+        numFilas = sentencia.executeUpdate(orden);
+        sentencia.close();
+        cerrarConeccion(con);
+        return numFilas;
+    }
+
+    public int eliminar(String condicion)throws SQLException{
+        int numFilas = 0;
+        Connection con = getConeccion();
+
+        String orden = "DELETE FROM cliente WHERE "+condicion;
+
+        Statement sentencia = con.createStatement();
+        numFilas = sentencia.executeUpdate(orden);
+        sentencia.close();
+        cerrarConeccion(con);
+        return numFilas;
+    }
+
+    public int modificar(Cliente e, String condicion)throws SQLException {
+        int numFilas = 0;
+        Connection con = getConeccion();
+
+        String orden = "UPDATE cliente SET " +
+                "nombre='"+e.getNombre()+"',"+
+                "fechaIngreso " + e.getFechaIngreso().toString() + "',"+
+                "fechaSslida = "+ e.isActivo()+ "',"+
+                "numhab = "+ e.getNumHab()+ "',"+ 
+                "comida = "+e.getComida()+"',"+
+                "correo = "+e.getCorreo()+
+                " WHERE "+condicion;
+
+        Statement sentencia = con.createStatement();
+        numFilas = sentencia.executeUpdate(orden);
+        sentencia.close();
+        cerrarConeccion(con);
+        return numFilas;
+    }
+
+    public ArrayList<Cliente> consultar(String condicion) throws SQLException{
+        ArrayList<Cliente> lista = new ArrayList<Cliente>();
+        Cliente e;
+        Connection con = getConeccion();
+        String orden = "SELECT * FROM hotel.cliente " +
+                (condicion==null || condicion.length()==0 ? "":"WHERE " + condicion);
+        Statement sentencia = con.createStatement();
+        ResultSet rs = sentencia.executeQuery( orden );
+        while (rs.next()) {
+            e = new Cliente(rs.getInt("clave"), rs.getString("nombre"),rs.getDate("fechaIngreso"),rs.getDate("fechaSalida"),rs.getInt("numhab"),rs.getInt("comida"),rs.getString("correo"));
+            lista.add( e );
+        }
+        sentencia.close();
+        cerrarConeccion(con);
+        return lista;
+    }
+}
